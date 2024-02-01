d1b16b47f2f309a2fcd4ad162b5dca0f4a05ec38
Noway-code <42259477+Noway-code@users.noreply.github.com>
2024-02-01 00:29:54 +0000

    Adjusted parameters

--- .ccls-cache/@home@runner@ColorlessVaguePup/vm.c
+++ .ccls-cache/@home@runner@ColorlessVaguePup/vm.c
@@ -2,27 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ARRAY_SIZE 499
+#define ARRAY_SIZE 499 // Define the size of the array
 
+// Initialize the base pointer, stack pointer, and program counter
 int BP = ARRAY_SIZE;
 int SP = ARRAY_SIZE + 1;
 int PC = 0;
 
-// Instruction
+// Define the structure for an instruction
 typedef struct myStruct {
-  int OP;
-  int L;
-  int M;
+  int OP; // Operation code
+  int L;  // Lexicographical level
+  int M;  // Modifier
 } myStruct; // for IR
 
+// Define the structure for a stack
 typedef struct stack {
-  int stack[ARRAY_SIZE];
-  int top;
+  int *stack; // Array of integers
+  int top;    // Index of the top element
 } stack;
 
 // Global variable declaration
 int pas[ARRAY_SIZE]; // Text and the stack will be in the same ARRAY
 
+// Function to calculate the base address of the activation record
 int base(int BP, int L) { // Find base L levels down
   int arb = BP; // arb = activation record base
   while (L > 0) // find base L levels down
@@ -33,18 +36,19 @@ int base(int BP, int L) { // Find base L levels down
   return arb;
 }
 
-
+// Main function
 int main(int argc, char *argv[]) { // impliment command line arguments
   int i = 0, Operation = 0, top = -1, instrCount = 0;
   char string[256];
   myStruct IR;
 
+  // Initialize the array to 0
   for (i = 0; i < ARRAY_SIZE; i++) {
     pas[i] = 0;
   }
-
   FILE *inFile;
 
+  // Check if the input file is specified
   if (argc != 2) {
     printf("Usage: %s<input.txt>\n", argv[0]);
     return 1; // Exit the program if the input file is not specified
@@ -52,19 +56,21 @@ int main(int argc, char *argv[]) { // impliment command line arguments
   inFile = fopen(argv[1], "r");
   // At this point, argv[1] contains the name of the input file
   printf("Reading from file: %s\n", argv[1]);
-  
+
+  // Read instructions from the file
   while (fscanf(inFile, "%d", &pas[instrCount]) == 1) {
     instrCount++;
   }
 
   // for testing outputs
-  for (int j = 0; j < instrCount; j++) { 
+  for (int j = 0; j < instrCount; j++) {
     if (j % 3 == 0) {
       printf("\n");
     }
     printf("(%d) ", pas[j]);
   }
-  
+
+  // Check if the file is empty
   if (instrCount != 0) {
     instrCount /= 3;
   } else {
@@ -74,18 +80,18 @@ int main(int argc, char *argv[]) { // impliment command line arguments
   printf("\n");
   printf("Initial values: \n");
 
-  
-  printf("				        PC	BP	  SP	  stack\n");
-  printf("Initial values:	0	  499	  500\n");
-  
+  printf("\t\tPC\tBP\tSP\tstack\n");
+  printf("Initial values:\t0\t499\t500\n");
+
   int flag = 0; // for exiting while loop
   while (flag == 0) // SYS M=3 (HALT).. doesnt cover all cases
   {
       IR.OP = pas[PC];
       IR.L = pas[PC + 1];
       IR.M  = pas[PC + 2];
-      PC = PC + 3; 
+      PC = PC + 3;
 
+    // Switch case for different operations
     switch (Operation) {
     case 1:           // LIT (literal)
       SP = SP - 1;    // decrement stack pointer
@@ -135,7 +141,7 @@ int main(int argc, char *argv[]) { // impliment command line arguments
         scanf("%d", &pas[SP]);
         break;
       case 3: // HALT
-        // output for stack 
+        // output for stack
         flag = 1;
         break;
       }
