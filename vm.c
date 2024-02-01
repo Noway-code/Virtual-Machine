79431052a6f21ac0cfef37c65ad19c458e08b04e
noway <dragonfury1928@gmail.com>
2024-02-01 13:12:37 -0500

    Change: extracted execute cycle to function
    Execution was bloating the main method too much. We still have more computation that needs to happen so this helps clean up readability.

--- vm.c
+++ vm.c
@@ -35,6 +35,153 @@ int base(int BP, int L)
 	return arb;
 }
 
+int execute(myStruct IR, char *opCode, int *pInt)
+{
+	switch (IR.OP)
+	{
+		case 1:           // LIT (literal)
+			SP = SP - 1;
+			pas[SP] = IR.M; // pushes value M onto the stack
+			opCode = "LIT";
+			break;
+		case 2: // OPR (return/operator)(will do later)
+			SP = BP + 1;
+			BP = pas[SP - 2];
+			PC = pas[SP - 3];
+
+			switch (IR.M)
+			{
+
+				case 1: // ADD
+					pas[SP+1] = pas[SP+1] + pas[SP];
+					SP = SP + 1;
+					opCode = "ADD";
+					break;
+
+				case 2: // SUB
+					pas[SP+1] = pas[SP+1] - pas[SP];
+					SP = SP + 1;
+					opCode = "SUB";
+					break;
+
+				case 3: // MUL
+					pas[SP+1] = pas[SP+1] * pas[SP];
+					SP = SP + 1;
+					opCode = "MUL";
+					break;
+
+				case 4: // DIV
+					pas[SP+1] = pas[SP+1] / pas[SP];
+					SP = SP + 1;
+					opCode = "DIV";
+					break;
+
+				case 5: // EQL
+					pas[SP+1] = pas[SP+1] == pas[SP];
+					SP = SP + 1;
+					opCode = "EQL";
+					break;
+
+				case 6: // NEQ
+					pas[SP+1] = pas[SP+1] != pas[SP];
+					SP = SP + 1;
+					opCode = "NEQ";
+					break;
+
+				case 7: // LSS
+					pas[SP+1] = pas[SP+1] < pas[SP];
+					SP = SP + 1;
+					opCode = "LSS";
+					break;
+
+				case 8: // LEQ
+					pas[SP+1] = pas[SP+1] <= pas[SP];
+					SP = SP + 1;
+					opCode = "LEQ";
+					break;
+
+				case 9: // GTR
+
+					pas[SP+1] = pas[SP+1] > pas[SP];
+					SP = SP + 1;
+					opCode = "GTR";
+					break;
+
+				case 10: // GEQ
+					pas[SP+1] = pas[SP+1] >= pas[SP];
+					SP = SP + 1;
+					opCode = "GEQ";
+					break;
+
+				case 11: // ODD
+					pas[SP] = pas[SP] % 2;
+					opCode = "ODD";
+					break;
+
+			}
+			break;
+
+		case 3: // LOD (load)
+			SP = SP - 1;
+			pas[SP] = pas[base(BP, IR.L) - IR.M]; // review
+			opCode = "LOD";
+			break;
+		case 4: // STO (store)
+			pas[base(BP, IR.L) - IR.M] = pas[SP]; // loads the value in the stack location at offset M from the
+			// stack pointer (SP) into the data memory
+			SP = SP + 1; // increment stack pointer, one less AR
+			opCode = "STO";
+			break;
+		case 5:                         // CAL (Call procedure)
+			pas[SP - 1] = base(BP, IR.L); // static link
+			pas[SP - 2] = BP;             // dynamic link
+			pas[SP - 3] = PC;             // return address
+			BP = SP - 1;
+			PC = IR.M;
+			opCode = "CAL";
+			break;
+		case 6: // INC (Allocates m slots on the stack)
+			SP = SP - IR.M;
+			opCode = "INC";
+			break;
+		case 7: // JMP (Jumps to a new instruction)
+			PC = IR.M;
+			opCode = "JMP";
+			break;
+		case 8: // JPC (Jumps conditionally); review
+			if (SP == 0)
+			{
+				PC = IR.M;
+				SP = SP + 1;
+			}
+			opCode = "JPC";
+			break;
+		case 9: // SYS
+			switch (IR.M)
+			{
+				case 1: // WRITE, SIN
+					SP = SP + 1;
+					printf("Output Result is: %d\n", pas[SP]);
+					opCode = "SIN";
+					break;
+				case 2: // READ, SOU
+					printf("Enter an integer: ");
+					scanf("%d", &pas[SP]);
+					SP = SP - 1;
+					opCode = "SOU";
+					break;
+				case 3: // HALT, EOP (End of Program)
+					opCode = "EOP";
+					return 1;
+			}
+			break;
+		default:
+			printf("Error: Invalid operation code.\n");
+			return 1;
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int i = 0, instrCount = 0;
@@ -92,180 +239,41 @@ int main(int argc, char *argv[])
 	while (halt == 0) // SYS M=3 (HALT).. doesnt cover all cases
 	{
 		//TODO: Implement the instruction cycle. Currently output is not correct and skips over some instructions.
+		opCode = "";
+
+		//Fetch
 		IR.OP = pas[PC];
 		IR.L = pas[PC + 1];
 		IR.M = pas[PC + 2];
 		PC = PC + 3;
-		opCode = "";
 
-		switch (IR.OP)
-		{
-			case 1:           // LIT (literal)
-				SP = SP - 1;
-				pas[SP] = IR.M; // pushes value M onto the stack
-				opCode = "LIT";
-				break;
-			case 2: // OPR (return/operator)(will do later)
-				SP = BP + 1;
-				BP = pas[SP - 2];
-				PC = pas[SP - 3];
+		//Execute
+		halt = execute(IR, opCode, &halt);
 
-				switch (IR.M)
-				{
-
-					case 1: // ADD
-						pas[SP+1] = pas[SP+1] + pas[SP];
-						SP = SP + 1;
-						opCode = "ADD";
-						break;
-
-					case 2: // SUB
-						pas[SP+1] = pas[SP+1] - pas[SP];
-						SP = SP + 1;
-						opCode = "SUB";
-						break;
-
-					case 3: // MUL
-						pas[SP+1] = pas[SP+1] * pas[SP];
-						SP = SP + 1;
-						opCode = "MUL";
-						break;
-
-					case 4: // DIV
-						pas[SP+1] = pas[SP+1] / pas[SP];
-						SP = SP + 1;
-						opCode = "DIV";
-						break;
-
-					case 5: // EQL
-						pas[SP+1] = pas[SP+1] == pas[SP];
-						SP = SP + 1;
-						opCode = "EQL";
-						break;
-
-					case 6: // NEQ
-						pas[SP+1] = pas[SP+1] != pas[SP];
-						SP = SP + 1;
-						opCode = "NEQ";
-						break;
-
-					case 7: // LSS
-						pas[SP+1] = pas[SP+1] < pas[SP];
-						SP = SP + 1;
-						opCode = "LSS";
-						break;
-
-					case 8: // LEQ
-						pas[SP+1] = pas[SP+1] <= pas[SP];
-						SP = SP + 1;
-						opCode = "LEQ";
-						break;
-
-					case 9: // GTR
-
-						pas[SP+1] = pas[SP+1] > pas[SP];
-						SP = SP + 1;
-						opCode = "GTR";
-						break;
-
-					case 10: // GEQ
-						pas[SP+1] = pas[SP+1] >= pas[SP];
-						SP = SP + 1;
-						opCode = "GEQ";
-						break;
-
-					case 11: // ODD
-						pas[SP] = pas[SP] % 2;
-						opCode = "ODD";
-						break;
-
-				}
-				break;
-
-			case 3: // LOD (load)
-				SP = SP - 1;
-				pas[SP] = pas[base(BP, IR.L) - IR.M]; // review
-				opCode = "LOD";
-				break;
-			case 4: // STO (store)
-				pas[base(BP, IR.L) - IR.M] =
-						pas[SP]; // loads the value in the stack location at offset M from the
-				// stack pointer (SP) into the data memory
-				SP = SP + 1; // increment stack pointer, one less AR
-				opCode = "STO";
-				break;
-			case 5:                         // CAL (Call procedure)
-				pas[SP - 1] = base(BP, IR.L); // static link
-				pas[SP - 2] = BP;             // dynamic link
-				pas[SP - 3] = PC;             // return address
-				BP = SP - 1;
-				PC = IR.M;
-				opCode = "CAL";
-				break;
-			case 6: // INC (Allocates m slots on the stack)
-				SP = SP - IR.M;
-				opCode = "INC";
-				break;
-			case 7: // JMP (Jumps to a new instruction)
-				PC = IR.M;
-				opCode = "JMP";
-				break;
-			case 8: // JPC (Jumps conditionally); review
-				if (SP == 0)
-				{
-					PC = IR.M;
-					SP = SP + 1;
-				}
-				opCode = "JPC";
-				break;
-			case 9: // SYS
-				switch (IR.M)
-				{
-					case 1: // WRITE, SIN
-						SP = SP + 1;
-						printf("Output Result is: %d\n", pas[SP]);
-						opCode = "SIN";
-						break;
-					case 2: // READ, SOU
-						printf("Enter an integer: ");
-						scanf("%d", &pas[SP]);
-						SP = SP - 1;
-						opCode = "SOU";
-						break;
-					case 3: // HALT, EOP (End of Program)
-						halt = 1;
-						opCode = "EOP";
-						break;
-				}
-				break;
-			default:
-				printf("Error: Invalid operation code.\n");
-				fclose(inFile);
-
-				return 1;
-		}
-
+		//Print
 		printf("%-10s%-4d%-4d%-4d%-4d%-4d\t", opCode, IR.L, IR.M, PC, BP, SP);
 
-// Print stack elements from SP to BP
+		// Print stack elements from SP to BP
 		for (int i = SP; i < BP; i++)
 		{
 			printf("%d ", pas[i]);
 		}
 
-// Print separator if BP is not equal to SP
+		// Print separator if BP is not equal to SP
 		if (BP != SP)
 		{
 			printf("| ");
 		}
 
-// Print stack elements from BP to BP - 3
+		// Print stack elements from BP to BP - 3
 		for (int i = BP; i > BP - 3; i--)
 		{
 			printf("%d ", pas[i]);
 		}
 
 		printf("\n");
-
 	}
+	fclose(inFile);
+	return 0;
 }
+
